6450ea6bf3b5bd2bbed5dbefbdb09582ea1b3f32
otto-vector <otto-vector@yandex.ru>
2020-04-04 12:16:21 +0300

    gulp adding start

---
+++ gulpfile.js
@@ -0,0 +1,158 @@
+'use strict';
+
+/* пути к исходным файлам (src), к готовым файлам (build), а также к тем, за изменениями которых нужно наблюдать (watch) */
+var path = {
+    build: {
+        html: 'assets/build/',
+        js: 'assets/build/js/',
+        css: 'assets/build/css/',
+        img: 'assets/build/img/',
+        fonts: 'assets/build/fonts/',
+        jadeFrom: 'assets/src/jade/',
+        jadeToHtml: 'assets/src/',
+        json: 'assets/build/json',
+        php: 'assets/build/'
+    },
+    src: {
+        html: 'assets/src/*.html',
+        js: 'assets/src/js/main.js',
+        style: 'assets/src/style/main.scss',
+        img: 'assets/src/img/**/*.*',
+        fonts: 'assets/src/fonts/**/*.*',
+        jade: 'assets/src/jade/**/*.jade',
+        json: 'assets/src/json/**/*.json',
+        php: 'assets/src/**/*.php'
+    },
+    watch: {
+        html: 'assets/src/**/*.html',
+        js: 'assets/src/js/**/*.js',
+        css: 'assets/src/style/**/*.+(scss|sass)',
+        img: 'assets/src/img/**/*.*',
+        fonts: 'assets/srs/fonts/**/*.*',
+        jade: 'assets/src/jade/**/*.jade',
+        json: 'assets/src/json/**/*.json',
+        php: 'assets/src/**/*.php'
+    },
+    clean: './assets/build/*'
+};
+
+/* настройки сервера */
+var config = {
+//    настройка удалена для работы proxy.php
+    server: {
+        baseDir: './assets/build'
+    },
+//    proxy: 'ptest.ru',
+    notify: false
+};
+
+/* подключаем gulp и плагины */
+var gulp = require('gulp'),  // подключаем Gulp
+    webserver = require('browser-sync'), // сервер для работы и автоматического обновления страниц
+    plumber = require('gulp-plumber'), // модуль для отслеживания ошибок
+    rigger = require('gulp-rigger'), // модуль для импорта содержимого одного файла в другой
+    sass = require('gulp-sass'), // модуль для компиляции SASS (SCSS) в CSS
+    autoprefixer = require('gulp-autoprefixer'), // модуль для автоматической установки автопрефиксов
+    cleanCSS = require('gulp-clean-css'), // плагин для минимизации CSS
+    uglify = require('gulp-uglify'), // модуль для минимизации JavaScript
+    cache = require('gulp-cache'), // модуль для кэширования
+    imagemin = require('gulp-imagemin'), // плагин для сжатия PNG, JPEG, GIF и SVG изображений
+    jpegrecompress = require('imagemin-jpeg-recompress'), // плагин для сжатия jpeg	
+    pngquant = require('imagemin-pngquant'), // плагин для сжатия png
+    rimraf = require('gulp-rimraf'), // плагин для удаления файлов и каталогов
+    rename = require('gulp-rename'),
+    jade = require('gulp-pug'), // плагин для конвертации JADE/PUG в HTML
+    jadeInheritance = require('gulp-pug-inheritance'), // аппаратная поддержка конвертации при больших объёмах файлов
+    jsonminify = require('gulp-jsonminify'), // минификация json файлов
+    htmlmin = require('gulp-htmlmin'); // минификатор html
+
+/* задачи */
+
+// запуск сервера
+gulp.task('webserver', function () {
+    webserver(config);
+});
+
+// сбор html
+gulp.task('html:build', function () {
+    return gulp.src(path.src.html) // выбор всех html файлов по указанному пути
+        .pipe(plumber()) // отслеживание ошибок
+        .pipe(rigger()) // импорт вложений
+        .pipe(htmlmin({ collapseWhitespace: true }))//минификация html
+        .pipe(gulp.dest(path.build.html)) // выкладывание готовых файлов
+        .pipe(webserver.reload({ stream: true })); // перезагрузка сервера
+});
+
+// сбор стилей
+gulp.task('css:build', function () {
+    return gulp.src(path.src.style) // получим main.scss
+        .pipe(plumber()) // для отслеживания ошибок
+        .pipe(sass()) // scss -> css
+        .pipe(autoprefixer()) // добавим префиксы
+        .pipe(gulp.dest(path.build.css))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cleanCSS()) // минимизируем CSS
+        .pipe(gulp.dest(path.build.css)) // выгружаем в build
+        .pipe(webserver.reload({ stream: true })); // перезагрузим сервер
+});
+
+// сбор js
+gulp.task('js:build', function () {
+    return gulp.src(path.src.js) // получим файл main.js
+        .pipe(plumber()) // для отслеживания ошибок
+        .pipe(rigger()) // импортируем все указанные файлы в main.js
+        .pipe(gulp.dest(path.build.js))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify()) // минимизируем js
+        .pipe(gulp.dest(path.build.js)) // положим готовый файл
+        .pipe(webserver.reload({ stream: true })); // перезагрузим сервер
+});
+
+//ФУНКЦИИ шрифтов и картинок ОТКЛЮЧЕНЫ
+//ПОТОМУ КАК В ДАННОМ ПРОЕКТЕ ПОКА НЕ ИСПОЛЬЗУЮТСЯ
+////////////////////////////////////////////////////////////////////
+// перенос шрифтов
+//  gulp.task('fonts:build', function () {
+//     return gulp.src(path.src.fonts)
+//         .pipe(gulp.dest(path.build.fonts));
+// });
+////////////////////////////////////////////////////////////////////
+
+// удаление каталога build 
+gulp.task('clean:build', function () {
+    return gulp.src(path.clean, { read: false })
+        .pipe(rimraf());
+});
+
+// очистка кэша для картинок, запускается вручную при изменении их количества
+gulp.task('cache:clear', function () {
+    cache.clearAll();
+});
+
+// сборка
+gulp.task('build',
+    gulp.series('clean:build',
+        gulp.parallel(
+            'jade:build',
+            'html:build',
+            'css:build',
+            'js:build',
+//            'fonts:build',
+        )
+    )
+);
+
+// запуск задач при изменении файлов
+gulp.task('watch', function () {
+    gulp.watch(path.watch.jade, gulp.series('jade:build'));
+    gulp.watch(path.watch.html, gulp.series('html:build'));
+    gulp.watch(path.watch.css, gulp.series('css:build'));
+    gulp.watch(path.watch.js, gulp.series('js:build'));
+//    gulp.watch(path.watch.fonts, gulp.series('fonts:build'));
+});
+
+// задача по умолчанию
+gulp.task('default', gulp.series(
+    'build',
+    gulp.parallel('webserver','watch')
+));
